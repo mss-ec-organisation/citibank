d2a20c15df8d504a2ea17ecc462241825d0e7a00
parteekbhardwaj <parteek.bhardwaj@gmail.com>
2023-04-24 10:15:47 +0530

    added git ignore file

--- Dbutils.java
+++ Dbutils.java
@@ -1,7 +1,10 @@
 
 hello edit some line ramu 
 hello this files contails the db related data 
+<<<<<<< HEAD
 adding some more lines to get the values from db 
 hello parteek how are you ?
 
+=======
+>>>>>>> parent of 33b32ea (updated..)
 
